03f348e7820f7fd8b033d596fc584305d257d49b
professoremerson <emerson@unc.br>
2023-04-03 20:11:20 -0300

    Instalação Biblioteca Ícones

---
+++ src/screens/login/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native'
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import IonicIcons from 'react-native-vector-icons/Ionicons'
